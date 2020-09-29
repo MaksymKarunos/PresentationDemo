4316a28874ea06dc95ec60a4cf979c68f68ec280
MaksymKarunos <maksim.karunos@gmail.com>
2020-09-29 13:31:24 -0400

    added unit tests

--- demo.c
+++ demo.c
@@ -1,36 +1,53 @@
-/******************************************************************************
-
-    @Author Maksym Karunos
-    @Credits: Prof. Jun Yuan, CS232
-    
-*******************************************************************************/
-
 #include <stdio.h>
-// Declaration of the function 
-void flip_bits(unsigned * x,
-             unsigned start,
-             unsigned end);
+#include <stdlib.h>
 
-int main()
-{
-    unsigned x = 1231232;
-    unsigned y = 1231232;
-    flip_bits(&x,0,4);
-    printf("%s"," Does x equal y? 0 - False, 1 - True: ");
-    printf("%d", x==y);
+// Flip the bits of x within range [start, end], in which both are inclusive.
+// Assume 0 <= start & end <= 31
+void flip_bits(unsigned * x,
+              unsigned start,
+              unsigned end) {
+    for ( int i = 0; i <= end - start; i++ ){
+        if ( ( *x & ( 1 << (i + start))) == 0){
+            *x = *x | ( 1 << (i + start));
+        }
+        else{
+            *x = *x & ~( 1 << (i + start));
+        }
+    }
+}
+
+
+
+
+void test_flip_bits(unsigned x,
+                   unsigned s,
+		   unsigned e,
+                   unsigned expected) {
+    unsigned o = x;
+    flip_bits(&x, s, e);
+    if(x!=expected) {
+        printf("flip_bits(0x%08x,%u,%u): 0x%08x, expected 0x%08x\n",o,s,e,x,expected);
+    } else {
+        printf("flip_bits(0x%08x,%u,%u): 0x%08x, correct\n",o,s,e,x);
+    }
+}
+
+int main(int argc,
+         const char * argv[]) {
+    printf("\nTesting flip_bits()\n\n");
+    test_flip_bits(0b1001110,0,0,0b1001111);
+    test_flip_bits(0b1001110,1,1,0b1001100);
+    test_flip_bits(0b1001110,2,2,0b1001010);
+    test_flip_bits(0b1001110,5,5,0b1101110);
+    test_flip_bits(0b1001110,9,9,0b1001001110);
+
+    test_flip_bits(0b1010,0,3,0b0101);
+    test_flip_bits(0b0101,0,3,0b1010);
+    test_flip_bits(0b0,0,0,0b1);
+    test_flip_bits(0b0,4,4,0b10000);
+    test_flip_bits(0b0,0,5,0b111111);
+    test_flip_bits(0b1111,0,4,0b10000);
+
+    printf("\n");
     return 0;
 }
-
-void flip_bits(unsigned * x,
-             unsigned start,
-             unsigned end) {
-   for ( int i = 0; i <= end - start; i++ ){
-       if (( *x & ( 1 << (i + start))) == 0){
-           *x = *x | ( 1 << (i + start));
-       }
-       else{
-           *x = *x & ~( 1 << (i + start));
-       }
-   }
-}
-
